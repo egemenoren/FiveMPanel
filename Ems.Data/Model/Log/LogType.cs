368ad9dac2b36506c8acfe56b83e1f3076a049ed
Egemen Ören <ebeyunamu@outlook.com>
2022-01-22 13:38:20 +0300

    logs accidentally ignored reCommit

---
+++ Ems.Data/Model/Log/LogType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ems.Data.Model.Log
+{
+    public enum LogType
+    {
+        Login = 1,
+        Logout = 2,
+        Insert = 3,
+        Update = 4,
+        Delete = 5
+    }
+}
---
+++ Ems.Data/Model/Log/Logs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ems.Data.Model.Log
+{
+    public class Logs : EmsBaseEntity
+    {
+        public int UserId { get; set; }
+        public string Mail { get; set; }
+        public string ValueEntered { get; set; }
+        public string Exception { get; set; }
+        public string StackTrace { get; set; }
+        public string Url { get; set; }
+        public LogLevel Level { get; set; }
+        public LogType Type { get; set; }
+    }
+}
